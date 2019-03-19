4db3ee5cb75e3264d9048ecfcb7f68284d818c94
Rocco Meli <rocco.meli@biodtp.ox.ac.uk>
2019-03-19 15:55:15 +0000

    plot.roc documentation

--- docs/source/index.rst
+++ docs/source/index.rst
@@ -3,8 +3,8 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to Scripts's documentation!
-===================================
+Scripts
+=======
 
 .. toctree::
    :maxdepth: 2
--- scripts/plot/roc.py
+++ scripts/plot/roc.py
@@ -1,18 +1,62 @@
 """
 Plot Receiver Operating Characteristic (ROC) curve.
 
-K-fold cross validation is supported.
+The ROC curve is obtained by plotting the *true positive rate* (TPR, also known as 
+*sensitivity* or *recall*)
+
+.. math::
+    \\text{TPR} = \\frac{\\text{TP}}{\\text{TP} + \\text{FN}}
+    
+versus the *false positive rate* (FPR, also known as *fall-out* or *probability of 
+false alarm*)
+
+.. math::
+    \\text{FPR} = \\frac{\\text{FP}}{\\text{FP} + \\text{TN}}
+
+where :math:`\\text{TP}`, :math:`\\text{TN}`, :math:`\\text{FP}`, and :math:`\\text{FN}`
+are the number of true positives, true negatives, false positives, and false negatives,
+respectively.
+
+.. note::
+    K-fold cross validation is supported.
 """
 
 import argparse as ap
 import numpy as np
 
-from typing import List, Optional
+from typing import Tuple, List, Optional
 
 from sklearn.metrics import roc_curve, roc_auc_score, auc
 
 from matplotlib import pyplot as plt
 
+
+def _roc_auc(fname: str) -> Tuple[np.array, np.array, float]:
+    """
+    Generate ROC curve and compute AUC
+
+    Args:
+        fname (str): Name of the data file
+
+    Returns:
+        Returns the false positive valuse (as ``np.array``), true positive values 
+        (as ``np.array``) and the AUC (as ``float``).
+
+    .. note::
+        The data file :param:`fname` is a two-column file containing the class ``y_true`` of 
+        the examples and their respective score ``y_score``.
+    """
+
+    # Load data from file
+    y_true, y_score = np.loadtxt(fname, unpack=True)
+
+    # Compute AUC
+    auc = roc_auc_score(y_true, y_score)
+
+    # Generate ROC curve
+    fpr, tpr, ths = roc_curve(y_true, y_score)
+
+    return fpr, tpr, auc
 
 def plot(fin: List[str], output: Optional[str] = None) -> None:
     """
@@ -25,6 +69,8 @@ def plot(fin: List[str], output: Optional[str] = None) -> None:
     .. note::
         If ``output`` is not specified, the plot is shown interactively.
     """
+
+    # Figure
     plt.figure()
     ax = plt.subplot(
         1,
@@ -38,30 +84,29 @@ def plot(fin: List[str], output: Optional[str] = None) -> None:
         ylabel="True Positive Rate",
     )
 
-    ax.plot([0, 1], [0, 1], "--", label="Random")
+    # Plot ROC for random classifier
+    ax.plot([0, 1], [0, 1], "--", label="Random", color="grey", lw=0.5)
 
-    if len(fin) == 1:
-        y_true, y_score = np.loadtxt(fin[0], unpack=True)
+    if len(fin) == 1: # Only one ROC
 
-        auc_score = roc_auc_score(y_true, y_score)
+        fpr, tpr, auc_score = _roc_auc(fin[0])
 
-        fpr, tpr, ths = roc_curve(y_true, y_score)
-
+        # Plot ROC
         ax.plot(fpr, tpr, label=f"AUC = {auc_score:.2f}")
 
-    else:
+    else: # Multiple ROCs (for different folds)
 
-        for idx, f in enumerate(fin):
-            y_true, y_score = np.loadtxt(f, unpack=True)
+        for idx, f in enumerate(fin): # Iterate over folds
 
-            auc_score = roc_auc_score(y_true, y_score)
+            fpr, tpr, auc_score = _roc_auc(fin[0])
 
-            fpr, tpr, ths = roc_curve(y_true, y_score)
-
+            # Plot ROC
             ax.plot(fpr, tpr, label=f"Fold {idx} (AUC = {auc_score:.2f})")
 
+    # Set legend
     ax.legend(loc="lower right")
 
+    # Plot or save
     if output is not None:
         plt.savefig(output)
     else:
@@ -83,7 +128,7 @@ def parse(args: Optional[str] = None) -> ap.Namespace:
     """
 
     # Parser
-    parser = ap.ArgumentParser(description="Plot ROC")
+    parser = ap.ArgumentParser(description="Plot ROC curve(s).")
 
     # Add arguments
     parser.add_argument("-i", "--input", nargs="+")
