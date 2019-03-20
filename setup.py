73083bdda5445786e9e998681b84a991f5610b2c
Rocco Meli <rocco.meli@biodtp.ox.ac.uk>
2019-03-20 12:59:26 +0000

    setup

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="scripts",
+    version="0.0",
+    author="Rocco Meli",
+    license="MIT",
+    description="",
+    long_description=long_description,
+    long_description_content_type="text/markdown", # GitHub-flavored Markdown
+    packages=setuptools.find_packages(),
+    install_requires=[],
+    python_requires=">=3.6",
+)
